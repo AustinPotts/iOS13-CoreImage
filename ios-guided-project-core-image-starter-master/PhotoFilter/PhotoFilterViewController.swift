b3fb34709a4b15d7112b092dd32ac30decf9a0d0
AustinPotts <pottsevolvements@gmail.com>
2020-03-09 12:15:57 -0400

    Configured

---
+++ ios-guided-project-core-image-starter-master/PhotoFilter/PhotoFilterViewController.swift
@@ -0,0 +1,43 @@
+import UIKit
+import CoreImage
+import Photos
+
+class PhotoFilterViewController: UIViewController {
+
+	@IBOutlet weak var brightnessSlider: UISlider!
+	@IBOutlet weak var contrastSlider: UISlider!
+	@IBOutlet weak var saturationSlider: UISlider!
+	@IBOutlet weak var imageView: UIImageView!
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+	}
+	
+	// MARK: Actions
+	
+	@IBAction func choosePhotoButtonPressed(_ sender: Any) {
+		// TODO: show the photo picker so we can choose on-device photos
+		// UIImagePickerController + Delegate
+	}
+	
+	@IBAction func savePhotoButtonPressed(_ sender: UIButton) {
+		// TODO: Save to photo library
+	}
+	
+
+	// MARK: Slider events
+	
+	@IBAction func brightnessChanged(_ sender: UISlider) {
+
+	}
+	
+	@IBAction func contrastChanged(_ sender: Any) {
+
+	}
+	
+	@IBAction func saturationChanged(_ sender: Any) {
+
+	}
+}
+
